82fde244c229fb4a7426c19bfc4ccbb9e18129fd
Raghu Betina <raghu@firstdraft.com>
2019-03-18 04:59:42 +0000

    Whitelist columns in Feedback for ActiveAdmin

--- app/admin/feedbacks.rb
+++ app/admin/feedbacks.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Feedback do
+
+ permit_params :task_id, :comment, :ratings
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
